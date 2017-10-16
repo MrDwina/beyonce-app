83866653203b46213faea22ea5a3c3a243b3a0e7
MrDwina <diwina1993@hotmail.com>
2017-10-16 22:53:51 +0100

    edit web details

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 
 const Footer = () => (
     <footer>
-        <h4> All &copy Copy Right Reserved To Music World Website 2017</h4>
+        <h4> All &copy Copy Right Reserved To Boncy Website 2017</h4>
     </footer>
 );
 
