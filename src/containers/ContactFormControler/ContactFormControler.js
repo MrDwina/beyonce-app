dbead0960b011595f7be5d7cc9a15cf6fb1ff940
MrDwina <diwina1993@hotmail.com>
2017-10-17 18:06:57 +0100

    change contact background color

--- src/containers/ContactFormControler/ContactFormControler.js
+++ src/containers/ContactFormControler/ContactFormControler.js
@@ -3,29 +3,14 @@ import "bootstrap/dist/css/bootstrap.css"
 import './ContactFormControler.css'
 class ContactFormControler extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             name: 'Put your full name',
             email: 'Put your name',
             message: 'Leave your message'
         }
-        this.handleChange = this.handleChange;
-        this.handleSubmit = this.handleSubmit;
     }
-
-    // handleChange(event) { 
-    //     this.setState({
-    //         name: event.target.value,
-    //         email: event.target.value,
-    //         message: event.target.value
-    //     });
-    // }
-
-    // handleSubmit(event) {
-    //     // alert('A name was submitted: ' + this.state.value);
-    //     event.preventDefault();
-    // }
 
     render() {
         return (
@@ -34,24 +19,24 @@ class ContactFormControler extends Component {
                 <h3>To contact us just put your details here and send them to us we wil replay as soon as possible</h3>
                 <div className="row jumbotron">
                     <div className="col-lg-8 col-lg-offset-2" >
-                        <form>
+                            <form>
                             <div className="row">
-                                <div className="col-lg-6 form-group">
+                                    <div className="col-lg-6 col-md-6 col-sm-6 form-group">
                                     <div className="row">
-                                        <div className="col-lg-2">
+                                            <div className="col-lg-2 col-md-2 col-sm-2">
                                             <label>Name:</label>
                                         </div>
-                                        <div className="col-lg-10">
+                                            <div className="col-lg-10 col-md-10 col-sm-10">
                                                 <input className="form-control" onChange={e => this.setState({ name: e.target.value })} type="text" value={this.state.name} placeholder={this.state.name} />
                                         </div>
                                     </div>                                                                                                         
                                 </div>
-                                <div className="col-lg-6">
+                                    <div className="col-lg-6 col-md-6 col-sm-6 form-group">
                                     <div className="row">
-                                        <div className="col-lg-2">
+                                            <div className="col-lg-2 col-md-2 col-sm-2">
                                             <label>Email:</label>
                                         </div>
-                                        <div className="col-lg-10">
+                                            <div className="col-lg-10 col-md-10 col-sm-10">
                                                 <input className="form-control" onChange={e => this.setState({ email: e.target.value })} type="email" value={this.state.email} placeholder={this.state.email} />
                                         </div>
                                     </div>                                                              
